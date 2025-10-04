170d72ac6b904de68022b5b0b4b20fc2bbd31f60
yusufbekxamidullayev <yusufbekxamidullayev37@gmail.com>
2025-10-04 22:25:39 +0500

    o'zgarish

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
