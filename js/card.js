a95ae9f83b536f280502559c664d9434a1631465
yusufbekxamidullayev <yusufbekxamidullayev37@gmail.com>
2025-10-20 22:45:27 +0500

    adding

--- js/card.js
+++ js/card.js
@@ -98,30 +98,29 @@ let idProduct = products.filter((el) => el.id == id);
 
 idProduct.map((el) => {
     singleProduct.innerHTML += `
-                    <div class="pt-[15px] flex flex-col gap-[40px] lg:flex lg:flex-row">
-                        <div class="flex  gap-[20px]">
-                            <div class="flex flex-col items-center gap-[5px]">
-                                ${
-                                    el.images.map((el) => {
-                                        return ` <img class="w-[100px] object-cover " src="${el}" alt="">`
-                                    }).join(" ")
+                    <div class="pt-[15px] flex flex-col gap-[40px] md:flex md:flex-row xl:gap-[200px]">
+                        <div class="flex flex-row justify-center gap-[0px] pr-[10px]  sm:flex-row gap-[20px]">
+                            <div class="px-[10px] flex flex-col gap-[8px] sm:flex sm:flex-col items-center gap-[5px]">
+                                ${el.images.map((el) => {
+                                    return ` <img class="w-[70px] sm:w-[100px]  object-cover " src="${el}" alt="">`
+                                }).join(" ")
                                 }
-                            </div>    
-                            <img class="w-[350px]" src="${el.images[0]}" alt="">
+                            </div>   
+                            <img class="w-[270px] sm:w-[350px]" src="${el.images[0]}" alt="">
                         </div>
                         <div>
-                            <div class="flex items-center gap-[300px]">
+                            <div class="flex items-center justify-between gap-[40px] px-[20px] sm:flex-row items-center  lg:flex-row items-center justify-between">
                                 <div>
                                     <p class="text-[24px] text-[#606060]">${el.price} ₽</p>
                                     <p class="text-[12px] text-[#BFBFBF]">Обычная цена</p>
                                 </div>
                                 <div>
-                                    <p class="text-[36px] text-[#414141] font-bold">${el.price - (el.price * el.discount) / 100 } ₽</p>
+                                    <p class="text-[36px] text-[#414141] font-bold">${el.price - (el.price * el.discount) / 100} ₽</p>
                                     <p class="text-[12px] text-[#BFBFBF]">С картой Северяночки</p>
                                 </div>
                             </div>
                             <div class="flex items-center justify-center pt-[16px]">
-                                <button class="w-[250px] h-[50px] bg-[#FF6633] rounded-[4px]">
+                                <button class="w-[250px] h-[50px] bg-[#FF6633] rounded-[4px] md:mt-[70px]">
                                     <a class="flex items-center justify-center gap-[20px]" href="">
                                         <img src="/assets/image/shopping.png" alt="savat">
                                         <p class="text-[#FFFFFF] text-[24px]">В корзину</p>
@@ -139,6 +138,6 @@ idProduct.map((el) => {
 
 let loadingScreen = document.getElementById("loading");
 
-window.addEventListener("load" , function(){
+window.addEventListener("load", function () {
     loadingScreen.classList.add("hidden")
 })
--- js/search.js
+++ js/search.js
@@ -19,11 +19,46 @@ search.addEventListener("input", function (e) {
 
     serachProduct.length === 0 ? searchCards.textContent = "Bunday mahsulot topilmadi.." : serachProduct.map((el) => {
         searchCards.innerHTML += `
-            <a href="../pages/product.html?id=${el.id}"
+            <a href="/товар.html?id=${el.id}"
             class="search-card  flex gap-[20px] items-center border-[2px] border-[greentellow] rounded-[5px] p-[3px]">
                             <img class="h-[80px] w-[100px] object cover" src=${el.images[0]} alt="">
                             <div>
                                 <h1>${el.name}</h1>
+                                <p>${el.description}</p>
+                            </div>
+             </a>
+        `
+    })
+})
+
+
+
+let input = document.getElementById("input")
+let inputCards = document.querySelector(".input-cards")
+
+let inputExist = false;
+inputExist ? inputCards.classList.remove("hidden") : inputCards.classList.add("hidden");
+
+
+input.addEventListener("input", function (e) {
+
+    inputCards.innerHTML = "";
+
+    let inputValue = e.target.value;
+    let inputProduct = products.filter((el) => el.name.toLowerCase().includes(inputValue.toLowerCase()))
+
+
+
+    inputValue ? inputExist = true : inputExist = false;
+    inputExist ? inputCards.classList.remove("hidden") : inputCards.classList.add("hidden");
+
+    inputProduct.length === 0 ? inputCards.textContent = "Bunday mahsulot topilmadi.." : inputProduct.map((el) => {
+        inputCards.innerHTML += `
+            <a href="/товар.html?id=${el.id}"
+            class="search-card  flex flex-col gap-[20px] items-center border-[2px] border-[greentellow] rounded-[5px] p-[3px]">
+                            <img class="h-[110px] w-full object cover" src=${el.images[0]} alt="">
+                            <div>
+                                <h1 class="font-bold">${el.name}</h1>
                                 <p>${el.description}</p>
                             </div>
              </a>
